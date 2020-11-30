0444e89ae303d2ded185a4bd6fa181294510501d
lheumell <leo.heumel@gmail.com>
2020-12-01 00:08:18 +0100

    First-commit

--- components/Recapitulatif.vue
+++ components/Recapitulatif.vue
@@ -1,24 +1,70 @@
 <template>
-     <v-expansion-panels class="mt">
-        <v-expansion-panel :key="i">
-          <v-expansion-panel-header>
-            Récapitulatif
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <p>1</p>
-            <p>2</p>
-            <p>3</p>
-            <p>4</p>
-            <p>5</p>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+  <v-expansion-panels class="mt" :info="info">
+    <v-expansion-panel>
+      <v-expansion-panel-header>
+        <h2>Récapitulatif</h2>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <p>
+          <span style="color: #7f8c8d ;">Type de materiel :</span>
+          {{ info.typeMateriel }}
+        </p>
+        <p>
+          <span style="color: #7f8c8d ;">Numero de chantier :</span>
+          {{ info.numeroChantier }}
+        </p>
+        <p>
+          <span style="color: #7f8c8d ;">Nom du chantier :</span>
+          {{ info.nomChantier }}
+        </p>
+        <p>
+          <span style="color: #7f8c8d ;">Adresse du chantier :</span>
+          {{ info.adresseChantier }}
+        </p>
+        <p>
+          <span style="color: #7f8c8d ;"
+            >Nom du chef de chantier (referent) :</span
+          >
+          {{ info.nomChef }}
+        </p>
+        <p>
+          <span style="color: #7f8c8d ;"
+            >Numero de téléphone du Chef de chantier :</span
+          >
+          {{ info.numChef }}
+        </p>
+
+        <p>
+          <span style="color: #7f8c8d ;">A partir du </span> {{ info.date }}
+          {{ info.time }} <span style="color: #7f8c8d ;"> pour </span>
+          {{ info.nbHeure
+          }}<span style="color: #7f8c8d ;"> {{ info.typeTemps }} </span>
+        </p>
+        <p v-if="info.chauffeur">
+          <span style="color: #7f8c8d ;">Chauffeur actuel :</span>
+          {{ info.chauffeur }}
+        </p>
+         <p v-if="info.tarif">
+          <span style="color: #7f8c8d ;">Tarif de l'engin :</span>
+          {{ info.tarif }}
+        </p>
+        <p>
+          <span style="color: #7f8c8d ;">Commentaires divers :</span>
+          {{ info.commentaire }}
+        </p>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    
-}
+  props: {
+    info: {}
+  }
+};
 </script>
 
 <style>
--- pages/list.vue
+++ pages/list.vue
@@ -1,13 +1,69 @@
 <template>
-  <div class="list">
-    <Item />
+  <div class="list_page">
+    <div class="list" v-if="showlist">
+      <li v-for="item in items" :key="item.id">
+        <v-container>
+          <button @click="updateSelected(item)" style="width: 100%">
+            <v-row justify="space-around">
+              <v-card width="100%">
+                <v-card-title class="ma-8">
+                  <p
+                    class="ml-3 "
+                    style="position: absolute;top: 50%; transform: translateY(-50%);"
+                  >
+                    N° de chantier : {{ item.id }}
+                  </p>
+
+                  <v-icon
+                    style="position:absolute; right: 1rem; top: 50%; transform: translateY(-50%); "
+                  >
+                    mdi-play</v-icon
+                  >
+                </v-card-title>
+
+                <p v-if="item.etat == 'Feuille de route validée'" class="green">
+                  {{ item.etat }}
+                </p>
+              </v-card>
+            </v-row>
+          </button>
+        </v-container>
+      </li>
+    </div>
+    <div v-else>
+      <Item :info="this.selectedItem" @goToBack="goToBack" />
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    props: {
-    items: null
-  }, 
+  data: () => ({
+    showlist: true,
+    selectedItem: {},
+    items: {}
+  }),
+  methods: {
+    goToBack() {
+      this.showlist = true;
+    },
+    updateSelected(selectedItem) {
+      this.selectedItem = selectedItem;
+      this.showlist = false;
+    }
+  },
+  mounted() {
+    axios
+      .get("http://localhost:8081/getFeuilleDeRoute")
+      .then(reponse => (this.items = reponse.data.feuilleDeRoute));
+  }
 };
 </script>
+
+<style lang="scss">
+li {
+  list-style: none;
+}
+</style>
