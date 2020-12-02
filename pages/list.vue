0ffa02a588d4ab9ee1c0dc2a42122a1e647c6709
lheumell <leo.heumel@gmail.com>
2020-12-02 18:16:00 +0100

    fixAPI

--- pages/list.vue
+++ pages/list.vue
@@ -57,7 +57,7 @@ export default {
   },
   mounted() {
     axios
-      .get("http://" + this.API_URL + "/getFeuilleDeRoute")
+      .get(this.API_URL + "/getFeuilleDeRoute")
       .then(reponse => (this.items = reponse.data.feuilleDeRoute));
   }
 };
